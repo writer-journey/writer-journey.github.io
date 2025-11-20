cabe9354b94f37774387e18102fbc72c5bfb843c
writer-journey <reza.aasi66@gmail.com>
2025-11-21 01:58:40 +0330

    Update firebaseConfig.js

--- en/firebaseConfig.js
+++ en/firebaseConfig.js
@@ -1,1 +1,13 @@
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
